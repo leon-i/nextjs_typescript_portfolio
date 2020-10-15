51c58456e67c0925a1158334d72f358bf4e26713
leon-i <isakleon97@gmail.com>
2020-10-14 22:09:17 -0400

    Changed folder from common to skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Layout} from "../components/common";
+import {Layout} from "../components/skeleton";
 import {Hero, Projects} from "../components/content";
 
 export default function Home() {
