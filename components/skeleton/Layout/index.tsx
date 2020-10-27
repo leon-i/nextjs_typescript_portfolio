1989eea5496f4430ea368a96ad68670cf0973261
leon-i <isakleon97@gmail.com>
2020-10-27 12:40:54 -0400

    Updated favicon and meta tags

--- components/skeleton/Layout/index.tsx
+++ components/skeleton/Layout/index.tsx
@@ -16,6 +16,15 @@ export const Layout : React.FC<Props> = ({ children }) => (
                   content="Isak Leon | Software Engineer | Portfolio"/>
             <meta name="viewport"
                   content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+            <meta property='og:title' content='Isak Leon | Software Engineer' />
+            <meta property='og:type' content='website' />
+            <meta property='og:url' content='https://leon-i.netlify.app/' />
+            <meta property='og:image' content="/site.jpg" />
+            <meta property='og:description'
+                  content='Isak Leon | Software Engineer | Portfolio' />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
             <link rel='manifest' href='/manifest.json' />
         </Head>
         <GlobalStyle />
